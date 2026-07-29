896c6060087b4f48ec9ff7edc1813655e4caa2a2
BrewedBoba <cydizon99@protonmail.com>
2026-07-29 10:34:35 -0600

    Added game loop and constants.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from snakegame!")
+import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("lightgreen")
+        pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()

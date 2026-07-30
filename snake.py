3b28eabd04e19c5aa299e68b29485a371af8ccce
BrewedBoba <cydizon99@protonmail.com>
2026-07-30 14:48:46 -0600

    fixed bug where the snake can start moving in any direction

--- snake.py
+++ snake.py
@@ -5,25 +5,28 @@ from constants import CELL_SIZE, NUMBER_OF_CELL, SNAKE_SPEED
 class Snake:
     def __init__(self) -> None:
         self.snake_body = [pygame.Vector2(14, 14), pygame.Vector2(14, 15), pygame.Vector2(14, 16)]
-        self.direction = pygame.Vector2(0, 0)
+        self.direction = pygame.Vector2(0, 1)
         self.time_since_last_move = 0
+        self.has_move = False
 
     def draw(self, screen) -> None:
         for square in self.snake_body:
             squares = pygame.Rect(square.x * CELL_SIZE, square.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(screen, "darkgreen", squares)
-            print(self.snake_body[-1])
+
 
     def move(self) -> None:
-        if self.direction != pygame.Vector2(0, 0):
+        if self.has_move == True:
             new_head = self.snake_body[-1] + self.direction
             self.snake_body.append(new_head)
             self.snake_body.pop(0)
 
     def update(self, dt) -> None:
         # Adding deltaTime
-        self.time_since_last_move += dt
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.has_move = True
+        self.time_since_last_move += dt
         # Once time_since_last_move is bigger than the threshold, then the snake moves.
         if self.time_since_last_move > (CELL_SIZE / SNAKE_SPEED):
             self.move()

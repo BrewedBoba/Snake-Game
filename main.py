b4d15a649d05c35fe8a52dd8e4db2a71c2bc86ad
BrewedBoba <cydizon99@protonmail.com>
2026-08-05 12:20:22 -0600

    Added game over screen and game over loop

--- main.py
+++ main.py
@@ -3,40 +3,61 @@ import sys
 from constants import CELL_SIZE, SCREEN_HEIGHT, SCREEN_WIDTH
 from food import Food
 from snake import Snake
-from ui import show_score
+from ui import show_score, game_over_screen
 
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #Set the Window
     pygame.display.set_caption("Snake Game") #set the display caption
     clock = pygame.time.Clock()
-    dt = 0.0
-    snake = Snake()
-    score = 0
-    food = Food(snake.snake_body)
-    while True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+    while True: # outer loop
+
+        dt = 0.0
+        snake = Snake()
+        score = 0
+        game_over = False
+        food = Food(snake.snake_body)
+
+        while game_over != True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
 
 
-        snake.update(dt)
-        snake.teleport()
+            snake.update(dt)
+            snake.teleport()
 
-        if food.eaten(snake.snake_body[-1], snake.snake_body) == True:
-            snake.grow()
-            score += 1
+            if food.eaten(snake.snake_body[-1], snake.snake_body) == True:
+                snake.grow()
+                score += 1
 
-        if snake.collide_with_itself() == True:
-            sys.exit()
+            if snake.collide_with_itself() == True:
+                game_over = True
 
-        screen.fill("lightgreen")
-        snake.draw(screen)
-        food.draw(screen)
-        show_score("black", 36, score, screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+            screen.fill("lightgreen")
+            snake.draw(screen)
+            food.draw(screen)
+            show_score("black", 36, score, screen)
+            pygame.display.flip()
+            dt = clock.tick(60) / 1000
+
+        while game_over:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        game_over = False
+                    if event.key == pygame.K_q:
+                        sys.exit()
+
+            game_over_screen("darkred", 56, screen)
+
+            pygame.display.flip()
 
 if __name__ == "__main__":
     main()
--- ui.py
+++ ui.py
@@ -1,7 +1,14 @@
 import pygame
 
+from constants import NUMBER_OF_CELL, SCREEN_HEIGHT, SCREEN_WIDTH
+
 
 def show_score(color, size, score, screen):
     score_font = pygame.font.Font(None, size)
     score_surface = score_font.render("Score: " + str(score), True, color)
     screen.blit(score_surface, (5, 6))
+
+def game_over_screen(color, size, screen):
+    game_over_font = pygame.font.Font(None, size)
+    game_over_surface = game_over_font.render("GAME OVER", True, color)
+    screen.blit(game_over_surface, (SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3))
